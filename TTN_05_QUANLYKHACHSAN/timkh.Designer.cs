fbb161430d6755a6a2b9b710345924911b201e4a
hoangthongnguyen06 <hoangthongnguyen06@users.noreply.github.com>
2019-09-07 23:27:52 +0700

    Update timkh.Designer.cs

--- TTN_05_QUANLYKHACHSAN/timkh.Designer.cs
+++ TTN_05_QUANLYKHACHSAN/timkh.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Qlks
+namespace Qlks
 {
     partial class timkh
     {
@@ -38,107 +38,7 @@
             this.Namsinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Ngaydp = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Ngaytp = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.label2 = new System.Windows.Forms.Label();
-            this.bt_xoa = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.bt_ct = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // bt_tk
-            // 
-            this.bt_tk.BackColor = System.Drawing.Color.SteelBlue;
-            this.bt_tk.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bt_tk.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.bt_tk.Location = new System.Drawing.Point(496, 54);
-            this.bt_tk.Name = "bt_tk";
-            this.bt_tk.Size = new System.Drawing.Size(95, 32);
-            this.bt_tk.TabIndex = 0;
-            this.bt_tk.Text = "Search";
-            this.bt_tk.UseVisualStyleBackColor = false;
-            this.bt_tk.Click += new System.EventHandler(this.bt_tk_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.LightCoral;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(14, 60);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 19);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Import";
-            // 
-            // txt_tk
-            // 
-            this.txt_tk.Location = new System.Drawing.Point(233, 61);
-            this.txt_tk.Name = "txt_tk";
-            this.txt_tk.Size = new System.Drawing.Size(242, 20);
-            this.txt_tk.TabIndex = 2;
-            // 
-            // dg
-            // 
-            this.dg.AllowUserToAddRows = false;
-            this.dg.AllowUserToDeleteRows = false;
-            this.dg.AllowUserToOrderColumns = true;
-            this.dg.AllowUserToResizeColumns = false;
-            this.dg.AllowUserToResizeRows = false;
-            this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dg.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Maphong,
-            this.Tenkh,
-            this.Socm,
-            this.Namsinh,
-            this.Ngaydp,
-            this.Ngaytp});
-            this.dg.Location = new System.Drawing.Point(-2, 99);
-            this.dg.Name = "dg";
-            this.dg.Size = new System.Drawing.Size(784, 206);
-            this.dg.TabIndex = 3;
-            this.dg.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dg_CellClick);
-            this.dg.Click += new System.EventHandler(this.dg_Click);
-            // 
-            // Maphong
-            // 
-            this.Maphong.DataPropertyName = "MaPhong";
-            this.Maphong.HeaderText = "Mã Phòng";
-            this.Maphong.Name = "Maphong";
-            // 
-            // Tenkh
-            // 
-            this.Tenkh.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Tenkh.DataPropertyName = "TenKh";
-            this.Tenkh.HeaderText = "Tên Khách Hàng";
-            this.Tenkh.Name = "Tenkh";
-            // 
-            // Socm
-            // 
-            this.Socm.DataPropertyName = "SoCm";
-            this.Socm.HeaderText = "Số Cmnd";
-            this.Socm.Name = "Socm";
-            // 
-            // Namsinh
-            // 
-            this.Namsinh.DataPropertyName = "NamSinh";
-            this.Namsinh.HeaderText = "Năm Sinh";
-            this.Namsinh.Name = "Namsinh";
-            // 
-            // Ngaydp
-            // 
-            this.Ngaydp.DataPropertyName = "NgayDp";
-            this.Ngaydp.HeaderText = "Ngày Đặt Phòng";
-            this.Ngaydp.Name = "Ngaydp";
-            // 
-            // Ngaytp
-            // 
-            this.Ngaytp.DataPropertyName = "NgayTp";
-            this.Ngaytp.HeaderText = "Ngày Trả Phòng";
-            this.Ngaytp.Name = "Ngaytp";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
+                        this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.SeaGreen;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Honeydew;
