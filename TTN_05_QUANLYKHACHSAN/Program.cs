7e842e2072f95d3911d3284221d62afc48f333b2
LeukocytesTT <32612962+LeukocytesTT@users.noreply.github.com>
2019-09-09 23:34:06 +0700

    Updated

--- TTN_05_QUANLYKHACHSAN/Program.cs
+++ TTN_05_QUANLYKHACHSAN/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace TTN_05_QUANLYKHACHSAN
+namespace Qlks
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace TTN_05_QUANLYKHACHSAN
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DangNhap());
         }
     }
 }
