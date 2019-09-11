01355c7cd1d07aff70ec133993ee747f1134c77e
vuongnguyen98 <37323991+vuongnguyen98@users.noreply.github.com>
2019-09-11 23:42:42 +0700

    repaired

--- TTN_05_QUANLYKHACHSAN/DangNhap.cs
+++ TTN_05_QUANLYKHACHSAN/DangNhap.cs
@@ -32,50 +32,6 @@ namespace Qlks
             Application.Exit();
         }
 
-        private void bt_dn_Click(object sender, EventArgs e)
-        {
-            dt.Clear();
-            dt = cl.dangnhap(txt_tk.Text, txt_mk.Text);
-            if (!string.IsNullOrEmpty(txt_tk.Text))
-            {
-                if (!string.IsNullOrEmpty(txt_mk.Text))
-                {
-                    if (dt.Rows[0]["errcode"].ToString() == "0")
-                    {
-                        qlks.nguoi_dn = txt_tk.Text;
-                        tinhtien.nv = txt_tk.Text;
-                        txt_mk.Text = "";
-                        txt_tk.Text = "";
-                        txt_tk.Focus();
-                        this.Hide();
-                        qlks ql = new qlks();
-                       ql.ShowDialog();
-                    }
-
-                    else
-                    {
-                        MessageBox.Show("Sai Tài Kho?n Ho?c M?t kh?u", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        txt_tk.Text = "";
-                        txt_mk.Text = "";
-                        txt_tk.Focus();
-                    }  
-                }
-                else
-                {
-                    MessageBox.Show("Nh?p M?t kh?u", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    txt_mk.Focus();
-                }
-            }
-            else
-            {
-                MessageBox.Show("Nh?p Tài Kho?n", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                txt_tk.Focus();
-            }
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
+      
     }
 }
