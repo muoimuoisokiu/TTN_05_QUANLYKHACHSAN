62a4db018c8b303224e8020e11f59084328f09dc
vuongnguyen98 <37323991+vuongnguyen98@users.noreply.github.com>
2019-09-12 08:02:16 +0700

    repaired

--- TTN_05_QUANLYKHACHSAN/doimatkhau.cs
+++ TTN_05_QUANLYKHACHSAN/doimatkhau.cs
@@ -35,7 +35,50 @@ namespace Qlks
 
         private void bt_luu_Click(object sender, EventArgs e)
         {
-           
+            if(!string.IsNullOrEmpty(txt_mkc.Text))
+            {
+                if (!string.IsNullOrEmpty(txt_mkm.Text))
+                {
+                    if (!string.IsNullOrEmpty(txt_nl.Text))
+                    {
+                        if (txt_mkm.Text == txt_nl.Text)
+                        {
+                            dt = cl.doimatkhau(lb_tk.Text, txt_mkc.Text, txt_mkm.Text);
+                            if (dt.Rows[0]["errcode"].ToString() == "0")
+                            {
+                                MessageBox.Show("Đã Lưu", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                this.Close();
+                               
+                            }
+                            else if (dt.Rows[0]["errcode"].ToString() == "1")
+                            {
+                                MessageBox.Show("Lỗi", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                frm_load();
+                                dt.Clear();
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Mật khẩu nhập lại không chính xác", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Xác nhận mật khẩu", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        txt_nl.Focus();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Nhập mật khẩu mới", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txt_mkm.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nhập mật khẩu cũ", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txt_mkc.Focus();
+            }
         }
 
         private void bt_dong_Click(object sender, EventArgs e)
